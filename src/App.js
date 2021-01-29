af24052690c4ab4eb1e944b6d5d4ae27435499f7
b.ferreira <b.ferreira@ua.pt>
2021-01-29 18:26:32 +0000

    sec

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD7ypvrmJ68_hdqlcAB5SHEnkFhRiyBEDA",
-  authDomain: "superchat-5e3ab.firebaseapp.com",
-  projectId: "superchat-5e3ab",
-  storageBucket: "superchat-5e3ab.appspot.com",
-  messagingSenderId: "829794296299",
-  appId: "1:829794296299:web:0019bd15f54c5c86c339ae",
-  measurementId: "G-0GD8TJ3199"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 })
 
 const auth = firebase.auth();
